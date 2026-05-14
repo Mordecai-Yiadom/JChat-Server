9b115098a9b0e11380a010fc445010d00d4c1b3b
Mordecai Yiadom <mordeyiadom@gmail.com>
2026-05-14 13:01:57 -0400

    Fix bug where server was sending incorrect packet type

--- src/jchat/server/JChatServer.java
+++ src/jchat/server/JChatServer.java
@@ -213,7 +213,7 @@ public class JChatServer
     {
         try
         {
-            JChatProtocolUtil.sendJChatTCPPacket(JChatClientMessagePacket.create(
+            JChatProtocolUtil.sendJChatTCPPacket(JChatServerMessagePacket.create(
                     new JChatTextMessage(this.name, message)), user.getSocket());
             return true;
         }
